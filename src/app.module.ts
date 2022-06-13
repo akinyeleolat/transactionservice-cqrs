8d5f28a81467e1410aa0c05e021506926a8bbebf
Tosin Akinyele <akinyeleolat2005@gmail.com>
2022-06-13 03:39:59 +0100

    set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Transaction } from './entities/transaction';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 55000,
+      username: 'postgres',
+      password: 'postgrespw',
+      database: 'transaction_service',
+      entities: [Transaction],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
